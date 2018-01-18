8607a94ceb555df223b4194b61f97dd83c802e19
Nico Sommi <nicosommi@gmail.com>
2018-01-18 18:03:48 -0300

    Adds vscode extension v0.0.1

--- packages/smart-snippets-vscode/README.md
+++ packages/smart-snippets-vscode/README.md
@@ -1,65 +1,23 @@
-# smart-snippets-vscode README
+# smart-snippets-vscode
+Smart snippet support on vscode
 
-This is the README for your extension "smart-snippets-vscode". After writing up a brief description, we recommend including the following sections.
+##Configuration
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
+WIP - TBD:
 
 \!\[feature X\]\(images/feature-x.png\)
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+> Tip: TBD
 
 ## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+* `smartSnippets.workspace`: this is the path in which archetypes are going to be saved. If `${workspaceRoot}` is found on the string, it will be replaced by the workspace dir, but it can also be an absolute path.
 
 ## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+### 0.0.1
+- Support smart snippet update regions and save to workspace
+- Support workspace setting and on it `${workspaceRoot}`. Defaults to os.homedir
+- Shows messages with success / error status
--- packages/smart-snippets-vscode/src/extension.ts
+++ packages/smart-snippets-vscode/src/extension.ts
@@ -2,26 +2,61 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { saveSmartSnippet, updateFromSmartSnippet } from 'smart-snippets'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "smart-snippets-vscode" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    const getActiveEditor = () => {
+        const editor = vscode.window.activeTextEditor
+        if (!editor) {
+            vscode.window.showInformationMessage('No editor is active! You should be editing the smart snippet in order to save it to the workspace!')
+        }
+        return editor
+    }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    const handleError = (e: Error) => {
+        vscode.window.showErrorMessage(e.message)
+    }
+    const handleSuccess = (message = 'Success') => {
+        vscode.window.showInformationMessage(message)
+    }
+    const config = vscode.workspace.getConfiguration('smartSnippets')
+    let workspace: string = config.get('workspace') || 'default'
+    workspace = workspace.replace('${workspaceRoot}', vscode.workspace.rootPath)
+
+    let saveCommand = vscode.commands.registerCommand('extension.save', async () => {
+        vscode.window.showInformationMessage('workspace is ' + workspace)
+        const editor = getActiveEditor()
+        if(!editor) return
+
+        const file = editor.document.fileName
+        try {
+            await saveSmartSnippet(file, workspace)
+            handleSuccess('Archetype saved in the smart snippets workspace sucessfuly')
+        } catch(e) {
+            handleError(e)
+        }
+    });
+    let updateCommand = vscode.commands.registerCommand('extension.update', async () => {
+        const editor = getActiveEditor()
+        if(!editor) return
+        
+        const file = editor.document.fileName
+        try {
+            await updateFromSmartSnippet(file, workspace)
+            handleSuccess('File updated to the latest version of the archetype sucessfuly')
+        } catch(e) {
+            handleError(e)
+        }
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(saveCommand);
+    context.subscriptions.push(updateCommand);
 }
 
 // this method is called when your extension is deactivated
